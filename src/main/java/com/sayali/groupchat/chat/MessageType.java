10f92286ff4a5288415f1c411ae46d3a5db27f73
Sayali Bhosale <78062567+SayaliBhosale16@users.noreply.github.com>
2024-04-13 23:46:51 -0400

    code updated

---
+++ src/main/java/com/sayali/groupchat/chat/MessageType.java
@@ -0,0 +1,9 @@
+package com.sayali.groupchat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+
+}
---
+++ src/test/java/com/sayali/groupchat/GroupchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayali.groupchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
